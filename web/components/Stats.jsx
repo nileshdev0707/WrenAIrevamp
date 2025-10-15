f08028b6f66d9952a6d05ada470f3e99309b21a9
nilesh <nileshdev0707@gmail.com>
2025-10-15 11:33:27 +0530

    home page data teams sections mobile view fixed

--- web/components/Stats.jsx
+++ web/components/Stats.jsx
@@ -7,7 +7,7 @@ export default function Stats({ data }) {
   const stats = data.trustedByDataTeams[0] || {}
   return (
     <section className="lg:py-6 md:py-5 sm:py-3 py-1">
-      <div className="max-w-7xl mx-auto lg:px-6 sm:px-4 px-2">
+      <div className="max-w-7xl mx-auto lg:px-6 sm:px-4 px-4">
         <p className="sm:mt-4 max-w-2xl mx-auto mt-2 text-3xl sm:text-4xl text-center md:text-5xl font-medium tracking-tight md:py-4 py-2 leading-tight">
           {stats.title?.split(' ').map((word, index) => {
             const isWorldwide = word.toLowerCase() === 'worldwide'
@@ -35,13 +35,13 @@ export default function Stats({ data }) {
             </div>
           ))}
         </div>
-         <div className="grid sm:grid-cols-3 md:gap-6 sm:gap-4 gap-2 md:mt-9 sm:mt-6 mt-4"> 
+         <div className="grid md:grid-cols-3 sm:grid-cols-2 md:gap-6 sm:gap-5 gap-4 md:mt-9 sm:mt-6 mt-4"> 
            {stats?.teamsItem?.map((s, i) => (
-             <div key={i} className="md:p-8 sm:p-4 p-2 bg-white rounded-xl shadow-[0px_56px_232px_-56px_rgba(0,0,0,0.5)] flex flex-col justify-between">
-                 <div className="font-medium text-[#1E1E1E] md:mb-8 mb-4 xl:text-xl lg:text-lg md:text-base text-sm">{s.title}</div>
+             <div key={i} className="lg:px-8 lg:py-10 md:px-6 md:py-8 sm:px-5 sm:py-6 px-4 py-5 bg-white rounded-xl shadow-[0px_56px_232px_-56px_rgba(0,0,0,0.5)] flex flex-col justify-between">
+                 <div className="font-medium text-[#1E1E1E] md:mb-8 mb-6 xl:text-xl lg:text-lg md:text-base text-base">{s.title}</div>
                   <div>
-                    <div className="text-[#0B8EE5] lg:text-base md:text-sm text-xs font-bold mb-1">{s.name}</div>
-                    <div className="text-[#757575] lg:text-base md:text-sm text-xs">{s.role}</div>
+                    <div className="text-[#0B8EE5] lg:text-base md:text-sm text-sm font-bold mb-1">{s.name}</div>
+                    <div className="text-[#757575] lg:text-base md:text-sm text-sm">{s.role}</div>
                   </div>
              </div>
            ))}
