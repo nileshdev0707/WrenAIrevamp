489ffbbe0851fdf0f850db32275ffdfeaaf347d4
nilesh <nileshdev0707@gmail.com>
2025-10-27 18:33:16 +0530

    update css

--- web/components/pricing/tiers.jsx
+++ web/components/pricing/tiers.jsx
@@ -5,7 +5,7 @@ export default function Tiers({ tiers, billing, onSeeDetails }) {
   const getUrl = useLocalizedUrl();
 
   return (
-    <div className="mt-10 grid md:grid-cols-3 gap-6 max-w-6xl mx-auto px-3">
+    <div className="md:mt-16 mt-10 grid lg:grid-cols-3 md:grid-cols-2 gap-6 max-w-7xl mx-auto px-4">
       {tiers?.map((item, index) => (
         <div
           key={index}
